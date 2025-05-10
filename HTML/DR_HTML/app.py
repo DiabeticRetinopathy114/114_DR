644a2add7e3c0e09710482f32bb8ab902008d970
DiabeticRetinopathy114 <ddrrrr114@gmail.com>
2025-05-11 07:12:21 +0800

    .h5

--- HTML/DR_HTML/app.py
+++ HTML/DR_HTML/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS              # 允許跨域請求
-from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.models import load_model # type: ignore
 import io
 
 from utils import load_and_resize, preprocess, postprocess
@@ -9,7 +10,7 @@ app = Flask(__name__)
 CORS(app)  # 允許所有網域呼叫此 API (前端在不同 host 時用得到)
 
 # 1) 載入你的 Keras 模型
-MODEL_PATH = 'model/dr_model.h5'
+MODEL_PATH = 'model.h5'
 model = load_model(MODEL_PATH)
 print(f"Loaded model from {MODEL_PATH}")
 
